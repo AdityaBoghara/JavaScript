762587a42e31d650a4aa79d92ac558cc88ec863a
AdityaBoghara <bogharaaditya@gmail.com>
2024-05-15 15:40:48 +0530

    01

---
+++ 01. Basics/app.js
@@ -0,0 +1,7 @@
+// console is an object and it contains many methods like log, warn, table.
+
+console.log("hello world");
+
+console.table({ name: "Aditya", age: 20 });
+
+console.warn("hello world");
